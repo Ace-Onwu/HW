524b98d618d40d39a94b2262c0ee5b4ece896de8
Ace-Onwu <ayinnaonwu@yahoo.com>
2022-06-26 17:18:26 -0700

    Database API

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-document.querySelector('button').addEventListener('click', grab)
+//The user will enter a cocktail. Get a cocktail name, 
+//photo, and instructions and place them in the DOM
 
-
-function grab(){
-    let name = document.querySelector('input').value
-    const url = `https://api.nationalize.io?name=${name}`
-    fetch(url)
+//Event Listener: click button and a function occurs
+ 
+    fetch("https://api.thecatapi.com/v1/images/search")
     .then(res => res.json())
     .then(data =>{
-        console.log(data)
-        document.querySelector('p').innerText = data.country[0].country_id
-        
+        console.log(data) // console log the object 
+        document.querySelector('h2').innerText = data[0].height
+        document.querySelector('img').src = data[0].url
+        document.querySelector('h3').innerText = data[0].width
+       
     })
-    
-    .catch(err=>{
-        console.log(`error ${err}`)
+    .catch(err => { // if something goes wrong
+        console.log('error ${err}')
     })
-}
+
+
+
