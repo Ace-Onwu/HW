357c0898649ba93278e958c22165753e9ce9f00e
Ace-Onwu <ayinnaonwu@yahoo.com>
2022-06-26 17:06:41 -0700

    API Practice 3

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ document.querySelector('button').addEventListener('click', grab)
 
 function grab(){
     let name = document.querySelector('input').value
-    const url = `https://api.agify.io?name=${name}`
+    const url = `https://api.nationalize.io?name=${name}`
     fetch(url)
     .then(res => res.json())
     .then(data =>{
         console.log(data)
-        document.querySelector('p').innerText = data.age
+        document.querySelector('p').innerText = data.country[0].country_id
+        
     })
     
     .catch(err=>{
